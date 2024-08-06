5a0cb83b79ee576bbfb1153f9b02b0dbbd0bb5a7
delpher <a.onashyuk@itu.int>
2024-08-06 12:01:11 +0300

    Added test for multiple instances of same context

--- UiFramework.Tests/UseContextHookShould.cs
+++ UiFramework.Tests/UseContextHookShould.cs
@@ -55,4 +55,49 @@ public class UseContextHookShould
             );
         }
     }
+
+    [Fact]
+    public void Maintain_Different_Values_Fro_Multiple_Instances()
+    {
+        var TestContext = CreateContext();
+
+        Render(CreateElement(App));
+
+        Root<object[]>().Should().SatisfyRespectively(
+            first => first.As<TextViewModel>().Text.Should().Be("state 1"),
+            second => second.As<TextViewModel>().Text.Should().Be("state 2"),
+            third => third.As<ButtonViewModel>().Should().NotBeNull()
+        );
+
+        Root<object[]>()[2].As<ButtonViewModel>().Click.Execute(null);
+
+        Root<object[]>().Should().SatisfyRespectively(
+            first => first.As<TextViewModel>().Text.Should().Be("state 1"),
+            second => second.As<TextViewModel>().Text.Should().Be("state 2"),
+            third => third.As<ButtonViewModel>().Should().NotBeNull()
+        );
+
+        return;
+
+        ElementFactory TestContextConsumer(dynamic? props, ElementFactory?[] children)
+        {
+            var value = UseContext(TestContext);
+            return CreateElement(Text, new { text = value });
+        }
+
+        ElementFactory App(dynamic? props, ElementFactory?[] children)
+        {
+            var (version, setVersion) = UseState(0);
+
+            return CreateElement(Framework.Fragment, null,
+                CreateElement(TestContext.Provider, new {value = "state 1"},
+                        CreateElement(TestContextConsumer)
+                    ),
+                CreateElement(TestContext.Provider, new {value = "state 2"},
+                    CreateElement(TestContextConsumer)
+                ),
+                CreateElement(Button, new { onClick = new Action(() => setVersion(version + 1))})
+            );
+        }
+    }
 }
