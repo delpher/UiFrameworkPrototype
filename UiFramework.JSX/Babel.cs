31f323b87c9910e0342fcd46e693e89496caf7fa
delpher <a.onashyuk@itu.int>
2024-08-06 10:43:16 +0300

    Optimized Babel transformations;

--- UiFramework.JSX/Babel.cs
+++ UiFramework.JSX/Babel.cs
@@ -5,12 +5,20 @@ namespace UiFramework.JSX;
 public class Babel : IDisposable
 {
     private readonly V8ScriptEngine _engine = new();
+    private bool _scriptInitialized;
 
     public string Transform(string sourceJs)
     {
-        _engine.Execute(ScriptResources.Read("babel.min.js"));
+        EnsureBabel();
         _engine.AddHostObject("ScriptSource", new { script = sourceJs });
         return (string)_engine.Evaluate("Babel.transform(ScriptSource.script, {presets: ['react','env']}).code");
+    }
+
+    private void EnsureBabel()
+    {
+        if (_scriptInitialized) return;
+        _engine.Execute(ScriptResources.Read("babel.min.js"));
+        _scriptInitialized = true;
     }
 
     public void Dispose()
